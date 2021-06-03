2172a4f1dd3f728964085196417ca846fa9caf20
BlackVision <19106267@vstu.ru>
2021-06-03 16:00:38 +0300

    Добавлена функция FractionReduction
    Была добавлена функция FractionReduction, которая выполняет сокращение дроби.

--- KNPO_RomanFractionReduction/RomanFractionReduction/RomanFractionReduction.cpp
+++ KNPO_RomanFractionReduction/RomanFractionReduction/RomanFractionReduction.cpp
@@ -6,3 +6,28 @@ int main()
 {
 
 }
+
+/*! Сокращение дроби
+    \param[in] numerator числитель дроби
+    \param[in] denominator знаменатель дроби
+    \param[out] массив arr заполненый сокращенными числами
+*/
+void FractionReduction(int arr[], int numerator, int denominator)
+{
+    int remainder; // Остаток от деления
+    int NOD; // Наибольший общий делитель
+    int a = numerator; // Числитель
+    int b = denominator; // Знаменатель
+
+    // Нахождение НОД
+    while (b) // Пока остаток от деления не будет равен 0
+    {
+        remainder = a % b; // Получаем остаток от деления
+        a = b; // Числитель заменяем на знаменатель
+        b = remainder; // Знаменатель заменяем на остаток от деления
+    }
+    NOD = a; // Найденный НОД
+    arr[0] = numerator / NOD; // Сокращаем числитель
+    arr[1] = denominator / NOD; // Сокращаем знаменатель
+
+}
